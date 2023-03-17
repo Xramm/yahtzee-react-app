782fcf1372fc162bbbecbe05dd2e02a7028705dd
juliusli <julius.eliel.liias@gmail.com>
2023-03-17 18:25:36 +0200

    Player order modification added and home changes.

--- components/PlayerListItem.tsx
+++ components/PlayerListItem.tsx
@@ -1,6 +1,15 @@
 import { CardTitle } from "@rneui/base/dist/Card/Card.Title";
-import { Dialog, Input, Card, Button, Chip } from "@rneui/themed";
+import {
+  Dialog,
+  Input,
+  Card,
+  Button,
+  Chip,
+  ListItem,
+  ButtonGroup,
+} from "@rneui/themed";
 import React, { useContext, useState } from "react";
+import { Alert } from "react-native";
 import MainContext from "../contexts/MainContext";
 import Player from "../interfaces/Player";
 import { warningColor } from "../utils/colors";
@@ -12,6 +21,7 @@ const PlayerListItem = (props: { player: Player }) => {
 
   const [editDialogIsVisible, setEditDialogIsVisible] = useState(false);
 
+  // Changed by the name input
   let playerEditName = "";
 
   // Toggles this player's editing dialog
@@ -21,7 +31,6 @@ const PlayerListItem = (props: { player: Player }) => {
 
   // Called when this player's remove button has been pressed
   const onRemovePressed = () => {
-
     // Filter this player out of the list
     const playersCopy = players.filter((playerInstance: Player) => {
       if (playerInstance === player) {
@@ -50,14 +59,16 @@ const PlayerListItem = (props: { player: Player }) => {
 
   // Called when the user presses save after editing this player's info
   const onSavePressed = () => {
-
     // Close the dialog and don't change anything if the name value wasn't changed
-    if (playerEditName === '') {
-        toggleEditDialog();
-        return;
+    if (playerEditName === "") {
+      Alert.alert("No new name given.", "Name will not changed.", [
+        { text: "Ok", onPress: toggleEditDialog },
+      ]);
+      return;
     }
-    
-    const playersCopy = players;
+
+    // Copy players list
+    const playersCopy = [...players];
 
     // Find this player in the list and set its name to a new one
     playersCopy.map((playerInstance: Player) => {
@@ -66,17 +77,64 @@ const PlayerListItem = (props: { player: Player }) => {
       }
     });
 
+    // Reassign the players list
     setPlayers(playersCopy);
 
+    // Toggle dialog (off)
     toggleEditDialog();
+  };
+
+  const movePlayerID = (amount: number) => {
+    // Check if player is already at the bottom or top of the list
+    if (player.id + amount >= players.length || player.id + amount < 0) {
+      return;
+    }
+
+    //Copy players list
+    const playersCopy = [...players];
+
+    // Temp save the names for both players before swap
+    const thisPlayerName = player.name;
+    const targetPlayerName = playersCopy[player.id + amount].name;
+
+    // Swap names
+    playersCopy[player.id].name = targetPlayerName;
+    playersCopy[player.id + amount].name = thisPlayerName;
+
+    // Update the player list
+    setPlayers(playersCopy);
+  };
+
+  // Called whenever one of the button group's buttons gets pressed
+  const onPlayerButtonGroupPress = (index: number) => {
+    switch (index) {
+      case 0: {
+        // Edit
+        toggleEditDialog();
+        break;
+      }
+      case 1: {
+        // Down
+        movePlayerID(1);
+        break;
+      }
+      case 2: {
+        //Up
+        movePlayerID(-1);
+        break;
+      }
+    }
   };
 
   return (
     <>
-      <Chip
-        title={player.name + " " + player.id}
-        onPress={toggleEditDialog}
-      />
+      <ListItem>
+        <ListItem.Title>{player.name + " " + player.id}</ListItem.Title>
+        <ListItem.ButtonGroup
+          buttons={["Edit", "Down", "Up"]}
+          onPress={onPlayerButtonGroupPress}
+        />
+      </ListItem>
 
       <Card.Divider width={0} />
 
@@ -86,7 +144,11 @@ const PlayerListItem = (props: { player: Player }) => {
       >
         <Dialog.Title title="Edit Player" />
 
-        <Input placeholder={player.name} onChangeText={onEditNameChangeText} />
+        <Input
+          placeholder={player.name}
+          onSubmitEditing={onSavePressed}
+          onChangeText={onEditNameChangeText}
+        />
 
         <Button title="Remove" onPress={onRemovePressed} color={warningColor} />
 
--- views/Home.tsx
+++ views/Home.tsx
@@ -1,6 +1,13 @@
 import { CardTitle } from "@rneui/base/dist/Card/Card.Title";
-import { Dialog, Input, Card, Button, Chip } from "@rneui/themed";
-import React, { useContext, useState } from "react";
+import { Dialog, Input, Card, Button, Chip, Text } from "@rneui/themed";
+import React, {
+  createRef,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { Alert, ScrollView } from "react-native";
 import PlayerListItem from "../components/PlayerListItem";
 import MainContext from "../contexts/MainContext";
 import Player from "../interfaces/Player";
@@ -11,6 +18,7 @@ const Home = ({ navigation }: any) => {
   const [addPlayerDialogIsVisible, setAddPlayerDialogIsVisible] =
     useState(false);
 
+  // Changed by the new player name input
   let newPlayerName = "";
 
   // Toggles the Add New Player dialog
@@ -25,16 +33,21 @@ const Home = ({ navigation }: any) => {
 
   // Called when a new player is being added into the player list
   const onAddPlayerSubmit = () => {
-    if (newPlayerName === '') {
-        toggleAddPlayerDialogVisible();
-        return;
-    };
+    // Deny new player if the name is null
+    if (newPlayerName === "") {
+      Alert.alert(
+        "Player name is empty",
+        "The player will not be added. Please enter a name to add a new player.",
+        [{ text: "Ok", onPress: toggleAddPlayerDialogVisible }]
+      );
+      return;
+    }
 
     // Create a new player object with the latest ID and the given name
-    const newPlayer = {id: players.length, name: newPlayerName}
+    const newPlayer = { id: players.length, name: newPlayerName };
 
     // Create a copy of the players, add the new player into it and set it as the new player list
-    const newPlayersList = players;
+    const newPlayersList = [...players];
     newPlayersList.push(newPlayer);
     setPlayers(newPlayersList);
 
@@ -43,7 +56,7 @@ const Home = ({ navigation }: any) => {
   };
 
   return (
-    <>
+    <ScrollView>
       <Card>
         <CardTitle>Players</CardTitle>
 
@@ -56,7 +69,12 @@ const Home = ({ navigation }: any) => {
 
       <Card>
         <Button
-          title="Start Game"
+          title={
+            players.length > 0
+              ? `Start ${players.length} Player Game`
+              : "Add Players To Start"
+          }
+          disabled={players.length <= 0}
           onPress={() => {
             navigation.navigate("Game");
           }}
@@ -69,11 +87,15 @@ const Home = ({ navigation }: any) => {
       >
         <Dialog.Title title="Add New Player" />
 
-        <Input placeholder="Player Name" onChangeText={onNewPlayerNameTextChange}/>
+        <Input
+          placeholder="Player Name"
+          onSubmitEditing={onAddPlayerSubmit}
+          onChangeText={onNewPlayerNameTextChange}
+        />
 
         <Button title="Add" onPress={onAddPlayerSubmit} />
       </Dialog>
-    </>
+    </ScrollView>
   );
 };
 
